4650cb731dffb0a28a16442ee335bf2054d4c763
917_Varo_Diana <72063179+VaroDiana917@users.noreply.github.com>
2023-01-08 21:36:58 +0200

    Modified toString

--- Lab5/Lab5/src/ParserOutput.java
+++ Lab5/Lab5/src/ParserOutput.java
@@ -82,35 +82,7 @@ public class ParserOutput {
         if(this.values.size() == 0){
             return sb.toString();
         }
-        sb.append(subtree(0));
         return sb.toString();
     }
 
-    private String subtree(int node) {
-        StringBuilder sb = new StringBuilder();
-        String value = values.get(node);
-        if(value.contains(" ")){
-            value = value.split(" ")[0];
-        }
-        sb.append(value).append("\n");
-        List<String> subtrees = new ArrayList<>();
-        int child = leftChild.get(node);
-        while (child != -1) {
-            String subtreeString = subtree(child);
-            String shiftedSubtree = Arrays.stream(subtreeString.split("\n")).map(l -> "\t" + l).reduce("", (s, t) -> s + t + "\n");
-            String linkedSubtree = " -- " + shiftedSubtree.substring(1);
-            subtrees.add(linkedSubtree);
-            child = rightSibling.get(child);
-        }
-        List<String> B = new ArrayList<>();
-        if (subtrees.size() > 0) {
-            for (String a : subtrees.subList(0, subtrees.size() - 1)) {
-                String b = Arrays.stream(a.split("\n")).map(l -> " \t|" + l.substring(1)).reduce("", (s, t) -> s + t + "\n");
-                B.add(b);
-            }
-            B.add(" \t\\" + subtrees.get(subtrees.size() - 1).substring(1));
-        }
-        B.forEach(sb::append);
-        return sb.toString();
-    }
 }
